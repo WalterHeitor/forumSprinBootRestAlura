f51d5536ed6b7d43f735a8db5aeb100b7f30fbcd
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-15 15:21:37 -0300

    Update TopicosController.java

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -5,20 +5,25 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDTO;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repositories.TopicoRepository;
 
 //@Controller
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 
 	@Autowired
 	private TopicoRepository topicoRepository;
-	@GetMapping("/topicos")
+	@GetMapping
 	public List<TopicoDTO> lista(String nomeCurso){
 		if(nomeCurso == null) {
 			List<Topico>topicos = topicoRepository.findAll();
@@ -28,4 +33,11 @@ public class TopicosController {
 			return TopicoDTO.converter(topicos);
 		}
 	}
+	
+	@PostMapping
+	public void cadastrar( @RequestBody TopicoForm form) { //anotaçao para indicar ao Spring que vai passar os parametros no corpo da requisiçao.
+		Topico topico = form.converter();
+		topicoRepository.save(topico);
+	}
+	
 }
