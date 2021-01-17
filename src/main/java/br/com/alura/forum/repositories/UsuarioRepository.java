a7fdf34577091ad6d123c68808491421b1216352
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-17 20:46:43 -0300

    Update UsuarioRepository.java

--- src/main/java/br/com/alura/forum/repositories/UsuarioRepository.java
+++ src/main/java/br/com/alura/forum/repositories/UsuarioRepository.java
@@ -3,11 +3,17 @@ package br.com.alura.forum.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import br.com.alura.forum.modelo.Usuario;
-
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	
 	Optional<Usuario> findByEmail(String email);
+
+	@Query(" SELECT u FROM Usuario u WHERE u.id = :idUsuario ")
+	Usuario findOne(@Param("idUsuario") Long idUsuario);
 
 }
