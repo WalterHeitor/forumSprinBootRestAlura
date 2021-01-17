e46b985f9b9c96ccd830de7d2505358d58d7b4b0
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-17 20:47:00 -0300

    Update AutenticacaoViaTokenFilter.java

--- src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
+++ src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
@@ -7,9 +7,24 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import br.com.alura.forum.modelo.Usuario;
+import br.com.alura.forum.repositories.UsuarioRepository;
+
 public class AutenticacaoViaTokenFilter extends OncePerRequestFilter{
+	
+	private TokenService tokenService;
+	private UsuarioRepository usuarioRepository;
+	
+
+	public AutenticacaoViaTokenFilter(TokenService tokenService,
+			UsuarioRepository usuarioRepository) {
+		this.tokenService = tokenService;
+		this.usuarioRepository = usuarioRepository;
+	}
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, 
@@ -17,13 +32,30 @@ public class AutenticacaoViaTokenFilter extends OncePerRequestFilter{
 			throws ServletException, IOException {
 		
 		String token = recuperarToken(request);
+		boolean valido = tokenService.isTokenValido(token);
 
+		if(valido) {
+			autenticarCliente(token);
+			System.out.println(valido);
+		}
+		
 		filterChain.doFilter(request, response);
+	}
+
+	private void autenticarCliente(String token) {
+		
+		Long idUsuario = tokenService.getIdUsuario(token);
+		Usuario usuario = usuarioRepository.findById(idUsuario).get();// se nao rodar implementar findOne ou findById().get()
+		System.out.println(usuario);
+		UsernamePasswordAuthenticationToken authentication = 
+				new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+		SecurityContextHolder.getContext().setAuthentication(authentication);;
+		
 	}
 
 	private String recuperarToken(HttpServletRequest request) {
 		String token = request.getHeader("Authorization");
-		if (token == null || token.isEmpty() || !token.startsWith("Beared")) {
+		if (token == null || token.isEmpty() || !token.startsWith("Bearer ")) {//nao esqueça do espaço.
 			return null;			
 		}
 		return token.substring(7, token.length());
