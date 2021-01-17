0b664d71140a1aeebdd6ac440fe668d7ced262da
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-17 00:56:07 -0300

    Update ForumApplication.java

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -1,16 +1,20 @@
 package br.com.alura.forum;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
+@EnableCaching
 public class ForumApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
-		System.out.println("erro");
+		System.out.println("Rodou beleza");
 	}
 
 }
