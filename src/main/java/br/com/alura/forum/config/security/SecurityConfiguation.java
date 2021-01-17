476b6214b29f445997999642195b538a409f501d
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-17 14:23:29 -0300

    Update SecurityConfiguation.java

--- src/main/java/br/com/alura/forum/config/security/SecurityConfiguation.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfiguation.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,16 +8,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguation extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private AutenticacaoService autenticacaoService;
 
 	//configuraçoes de autenticação.
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(auth);
+		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
 	}
 	//configuração de autorização por url
 	@Override
@@ -33,4 +37,9 @@ public class SecurityConfiguation extends WebSecurityConfigurerAdapter{
 		// TODO Auto-generated method stub
 		super.configure(web);
 	}
+	
+	/*public static void main(String[] args) {
+		System.out.println(new BCryptPasswordEncoder().encode("123456"));
+	}*/
+	
 }
