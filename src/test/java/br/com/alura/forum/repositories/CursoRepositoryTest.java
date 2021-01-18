26ed53de2de561f3b116f9706ba4cb77f3c585ef
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-18 11:59:27 -0300

    Update CursoRepositoryTest.java

--- src/test/java/br/com/alura/forum/repositories/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repositories/CursoRepositoryTest.java
@@ -1,19 +1,20 @@
 package br.com.alura.forum.repositories;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.alura.forum.modelo.Curso;
 
 @RunWith(SpringRunner.class)
-@
-DataJpaTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class CursoRepositoryTest {
 	
 	@Autowired
