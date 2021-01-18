2321e7b063b182e9f32373990ad85e0dfa8112dc
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-18 02:51:45 -0300

    Create DevSecurityConfiguation.java

---
+++ src/main/java/br/com/alura/forum/config/security/DevSecurityConfiguation.java
@@ -0,0 +1,25 @@
+package br.com.alura.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+@Profile("dev")
+public class DevSecurityConfiguation extends WebSecurityConfigurerAdapter{
+
+	//configuração de autorização por url
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/**").permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.csrf().disable();
+	}
+	
+	
+}
