0d0b43fbb01b2cf4b6eb0c8a00bbc0824a984f4b
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-18 14:37:20 -0300

    consetando erro do build

--- src/test/java/br/com/alura/forum/repositories/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repositories/CursoRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -19,14 +20,21 @@ class CursoRepositoryTest {
 
 	@Autowired
 	private CursoRepository repository;
-	/*
+	
 	@Autowired
 	private TestEntityManager em;
-	*/
+	
 
 	@Test
 	public void deveriaCarregarCursoPorNome() {
+		
+		
+		
 		String nomeCurso = "HTML 5";
+		Curso html5 = new Curso();
+		html5.setNome(nomeCurso);
+		html5.setCategoria("Programacao");
+		em.persist(html5);
 		Curso curso = repository.findByNome(nomeCurso);
 		Assert.assertNotNull(curso);
 		Assert.assertEquals(nomeCurso, curso.getNome());
